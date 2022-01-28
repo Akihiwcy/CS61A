ee7e5f1e3af07e5e10d99b8fd30058d38c6cd2e1
Akihiwcy <akihi.wcy@gmail.com>
2022-01-28 15:57:47 +0800

    Lab 8: Iterators, Generators, Object-Oriented Programming

---
+++ lab/lab08/lab08.py
@@ -0,0 +1,45 @@
+def make_generators_generator(g):
+    """Generates all the "sub"-generators of the generator returned by
+    the generator function g.
+
+    >>> def every_m_ints_to(n, m):
+    ...     i = 0
+    ...     while (i <= n):
+    ...         yield i
+    ...         i += m
+    ...
+    >>> def every_3_ints_to_10():
+    ...     for item in every_m_ints_to(10, 3):
+    ...         yield item
+    ...
+    >>> for gen in make_generators_generator(every_3_ints_to_10):
+    ...     print("Next Generator:")
+    ...     for item in gen:
+    ...         print(item)
+    ...
+    Next Generator:
+    0
+    Next Generator:
+    0
+    3
+    Next Generator:
+    0
+    3
+    6
+    Next Generator:
+    0
+    3
+    6
+    9
+    """
+    "*** YOUR CODE HERE ***"
+    tmp = list(g())
+    def g(cnt):
+        for i in range(cnt):
+            yield tmp[i]
+    for c in range(1, len(tmp)+1):
+        yield g(c)
+
+
+
+
